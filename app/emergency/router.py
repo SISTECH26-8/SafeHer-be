b50ae517e6bbfb39ee1eea8299fd42e2f8d8b8e9
lulultfh <punyalulu05@gmail.com>
2026-08-03 00:08:58 +0700

    feat(contact): create update and delete endpoint for emergency contact

--- app/emergency/router.py
+++ app/emergency/router.py
@@ -12,3 +12,11 @@ def create_emergency_contact(data: schemas.EmergencyContactCreate, db: Session =
 @router.get("/users/emergency-contacts", response_model=schemas.EmergencyContactListResponse, status_code=status.HTTP_200_OK)
 def get_emergency_contacts(db: Session = Depends(get_db), user_id: str = Depends(get_current_user_id)):
     return services.list_emergency_contacts(db=db, user_id=user_id)
+
+@router.put("/users/emergency-contacts/{contact_id}", response_model=schemas.EmergencyContactUpdateResponse, status_code=status.HTTP_200_OK)
+def update_emergency_contact(contact_id: str, data: schemas.EmergencyContactCreate, db: Session = Depends(get_db), user_id: str = Depends(get_current_user_id)):
+    return services.update_emergency_contact(db=db, user_id=user_id, contact_id=contact_id, data=data)
+
+@router.delete("/users/emergency-contacts/{contact_id}", response_model=schemas.EmergencyContactDeleteResponse, status_code=status.HTTP_200_OK)
+def delete_emergency_contact(contact_id: str, db: Session = Depends(get_db), user_id: str = Depends(get_current_user_id)):
+    return services.delete_emergency_contact(db=db, user_id=user_id, contact_id=contact_id)
