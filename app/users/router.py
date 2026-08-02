2f216b5bab0a64e9b828ae79a5ceea56624cd56d
lulultfh <punyalulu05@gmail.com>
2026-08-02 13:03:56 +0700

    refactor(contact): move auth prefix to router

--- app/users/router.py
+++ app/users/router.py
@@ -5,15 +5,15 @@ from app.users import schemas, services
 
 router = APIRouter(tags=["Authentication"])
 
-@router.post("/auth/register", response_model=schemas.RegisterResponse, status_code=status.HTTP_201_CREATED)
+@router.post("/register", response_model=schemas.RegisterResponse, status_code=status.HTTP_201_CREATED)
 def register(data: schemas.RegisterRequest, db: Session = Depends(get_db)):
     return services.register_user(db, data)
 
-@router.post("/auth/login", response_model=schemas.LoginResponse, status_code=status.HTTP_200_OK)
+@router.post("/login", response_model=schemas.LoginResponse, status_code=status.HTTP_200_OK)
 def login(data: schemas.LoginRequest, db: Session = Depends(get_db)):
     return services.login_user(db, data)
 
-@router.get("/auth/me", status_code=status.HTTP_200_OK)
+@router.get("/me", status_code=status.HTTP_200_OK)
 def get_current_user_info(user_id: str = Depends(get_current_user_id)):
     """Test endpoint to verify JWT token."""
     return {"user_id": user_id, "message": "Token Anda valid!"}
